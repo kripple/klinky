00c226b1bdbbca998b19719263a7787edf6b8c91
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-08-01 19:19:42 -0400

    add some config files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Klinky Link URL Shortener
---
+++ config/drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: 'src/backend/db/migrations',
+  schema: 'src/backend/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
