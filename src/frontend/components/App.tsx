3d0fca5bfb1ee9390194e9781fe8916281c2b0a7
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-08-05 11:53:56 -0400

    add basic desktop UI

--- src/frontend/components/App.tsx
+++ src/frontend/components/App.tsx
@@ -6,8 +6,56 @@ export function App() {
       <div className="fixed-background"></div>
       <div className="app-container">
         <div className="scroll-container">
-          <header>header</header>
-          <main>main</main>
+          <header className="header">
+            <div className="app-title">klinky.link</div>
+            <button>TBD</button>
+          </header>
+          <main className="main">
+            <h1 className="heading">
+              The anti-lytics
+              <span className="heading-highlight">URL Shortener</span>
+            </h1>
+            <h2 className="subheading">
+              Privacy isn’t just a feature — it’s the point.
+              <br />
+              No tracking. No accounts. Just links.
+            </h2>
+            <div className="card">
+              <form>
+                <div className="input-pair">
+                  <input
+                    className="readonly-prefix"
+                    readOnly
+                    tabIndex={-1}
+                    value="https://"
+                  ></input>
+                  <input
+                    className="input-value"
+                    placeholder="Enter link here"
+                    required
+                  ></input>
+                </div>
+                <div className="input-pair">
+                  <input
+                    className="readonly-prefix"
+                    readOnly
+                    tabIndex={-1}
+                    value="https://klinky.link/"
+                  ></input>
+                  <input
+                    className="input-value"
+                    placeholder="Customize your link (optional)"
+                  ></input>
+                </div>
+                <button type="submit">Shorten URL</button>
+              </form>
+            </div>
+            <div className="key-features-list">
+              <span>Free to use</span>
+              <span>Open source</span>
+              <span>No signup required</span>
+            </div>
+          </main>
         </div>
       </div>
     </>
