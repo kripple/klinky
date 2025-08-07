fe5df244c3ef54ad101c6db26a513bf219929a94
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-08-07 16:14:12 -0400

    add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { resolve } from 'path';
 
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-// import { visualizer } from 'rollup-plugin-visualizer';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { type PluginOption, defineConfig } from 'vite';
 
-const plugins: PluginOption[] = [react()];
+const enableVisualizer: boolean = false;
+const plugins: PluginOption[] = [tailwindcss(), react()];
 
 // visualizer must be last
-// plugins.push(visualizer({ open: false }));
+enableVisualizer && plugins.push(visualizer({ open: false }));
 
 // https://vitejs.dev/config/
 export default defineConfig({
