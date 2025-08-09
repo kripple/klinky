64f8f80f80076dac18ae3986a031ef354ff859a9
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-08-09 15:58:09 -0400

    animate success and loading responses

--- src/frontend/components/LinkForm.tsx
+++ src/frontend/components/LinkForm.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { api } from '@/frontend/api';
 import { Input } from '@/frontend/components/Input';
@@ -18,9 +18,69 @@ export function LinkForm({ user_uuid }: { user_uuid?: string }) {
   const [aliasErrors, setAliasErrors] = useState<string[]>([]);
   const [linkErrors, setLinkErrors] = useState<string[]>([]);
 
+  const minDelayMs = 800 as const;
+  const maxDelayMs = 1000 as const;
+  const [loading, setLoading] = useState<boolean>(false);
+  const [success, setSuccess] = useState<boolean>(false);
+
+  const successHandler = useRef<NodeJS.Timeout | null>(null);
+  const loadingHandler = useRef<NodeJS.Timeout | null>(null);
+  const delayedSuccessHandler = useRef<NodeJS.Timeout | null>(null);
+
   useEffect(() => {
-    console.log(response);
-  }, [response]);
+    const animateSuccess = () => {
+      if (response.isSuccess) {
+        setSuccess(true);
+
+        successHandler.current = setTimeout(() => {
+          setSuccess(false);
+        }, maxDelayMs);
+      }
+    };
+
+    if (response.isLoading) {
+      // Start a timer to delay showing loading state
+      loadingHandler.current = setTimeout(() => {
+        setLoading(true);
+
+        // Once loading shows, ensure it stays visible for min delay
+        delayedSuccessHandler.current = setTimeout(() => {
+          // After min loading time, if request already succeeded, show success
+          animateSuccess();
+        }, minDelayMs);
+      }, minDelayMs);
+    } else {
+      // Request not loading anymore, cancel timer in case it has not yet run
+      if (loadingHandler.current) {
+        clearTimeout(loadingHandler.current);
+        loadingHandler.current = null;
+      }
+      if (delayedSuccessHandler.current) {
+        clearTimeout(delayedSuccessHandler.current);
+        delayedSuccessHandler.current = null;
+      }
+
+      animateSuccess();
+    }
+
+    return () => {
+      [loadingHandler, successHandler, delayedSuccessHandler].forEach(
+        (handler) => handler.current && clearTimeout(handler.current),
+      );
+    };
+  }, [response.isLoading, response.isSuccess]);
+
+  useEffect(() => {
+    if (!response.isSuccess) return;
+
+    setSuccess(true);
+
+    const handler = setTimeout(() => {
+      setSuccess(false);
+    }, maxDelayMs);
+
+    return () => clearTimeout(handler);
+  }, [response.isSuccess]);
 
   const parseForm = ({
     alias,
@@ -46,6 +106,8 @@ export function LinkForm({ user_uuid }: { user_uuid?: string }) {
 
   const submit = (event: FormEvent) => {
     event.preventDefault();
+    if (response.isLoading) return;
+
     const data = new FormData(event.currentTarget);
     const params = parseForm({
       alias: data.get('alias'),
@@ -55,10 +117,27 @@ export function LinkForm({ user_uuid }: { user_uuid?: string }) {
     createLink(params);
   };
 
+  const checkmark = (
+    <svg
+      className="w-5 h-5"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5 13l4 4L19 7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={3}
+      />
+    </svg>
+  );
+
   return (
     <form
       autoComplete="off"
-      className="card bg-base-100 shadow-md m-6 p-6"
+      className="card bg-base-100 shadow-md m-6 p-6 border border-primary-content"
       onSubmit={submit}
     >
       <Input
@@ -81,11 +160,17 @@ export function LinkForm({ user_uuid }: { user_uuid?: string }) {
       />
 
       <button
-        className="btn btn-primary"
-        disabled={!user_uuid || response.isLoading || maxLinks}
+        className={`btn ${success ? 'btn-success' : 'btn-primary'}`}
+        disabled={!user_uuid || maxLinks}
         type="submit"
       >
-        Create Short Link
+        {success ? (
+          checkmark
+        ) : loading ? (
+          <span>Linking ...</span>
+        ) : (
+          <span>Create Short Link</span>
+        )}
       </button>
       {/* <p className="validator-hint">
         {maxLinks
