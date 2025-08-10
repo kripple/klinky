3871dab66614e61c4367b058302ee0380c985dc4
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-08-10 18:42:39 -0400

    sort links by most recent

---
+++ src/frontend/hooks/useSortedLinks.ts
@@ -0,0 +1,18 @@
+import { api } from '@/frontend/api';
+
+export const useSortedLinks = ({ user_uuid }: { user_uuid?: string }) => {
+  return api.useGetLinksQuery(
+    { user_uuid: user_uuid as string },
+    {
+      skip: !user_uuid,
+      selectFromResult: ({ currentData, ...result }) => ({
+        ...result,
+        currentData: currentData
+          ? [...currentData].sort((a, b) =>
+              a.updated_at < b.updated_at ? 1 : -1,
+            )
+          : [],
+      }),
+    },
+  );
+};
