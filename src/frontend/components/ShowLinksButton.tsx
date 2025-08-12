0986e6f316378acdb4c0101eef1023dc07945dec
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-08-11 21:12:39 -0400

    fix show links button

--- src/frontend/components/ShowLinksButton.tsx
+++ src/frontend/components/ShowLinksButton.tsx
@@ -1,4 +1,4 @@
-import { FaArrowDown as ArrowDown } from 'react-icons/fa6';
+import { FaArrowDown as ArrowDownIcon } from 'react-icons/fa6';
 
 export function ShowLinksButton({
   open,
@@ -13,42 +13,45 @@ export function ShowLinksButton({
 }) {
   const closeSvg = 'M6 18L18 6M6 6l12 12' as const;
   const openSvg = 'M12 4v16M4 12h16' as const;
+  const buttonStyle = 'btn btn-primary pl-3 pr-2 cursor-pointer' as const;
+  const desktopOnly = 'hidden lg:flex' as const;
+  const mobileOnly = 'flex lg:hidden' as const;
 
   const contents = (
     <>
       Links
-      <span className="badge badge-accent aspect-square rounded-full font-bold p-1 m-0 h-auto flex lg:hidden">
-        <ArrowDown />
-      </span>
-      <span
-        className={`badge aspect-square rounded-full font-bold p-1 m-0 h-auto ${open ? 'badge-accent' : 'badge-secondary'} hidden lg:flex`}
-      >
-        {open && count && count > 0 ? (
-          <span className="h-4 w-4 text-xs p-0">{count}</span>
-        ) : (
-          <svg
-            className="h-4 w-4"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d={open ? openSvg : closeSvg}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={3}
-            />
-          </svg>
-        )}
-      </span>
+      <ArrowDownIcon
+        className={`${mobileOnly} badge badge-accent aspect-square rounded-full p-1 m-0 cursor-pointer`}
+      />
+      {open && count && count > 0 ? (
+        <span
+          className={`${desktopOnly} badge badge-accent aspect-square rounded-full text-xs p-0`}
+        >
+          {count}
+        </span>
+      ) : (
+        <svg
+          className={`${desktopOnly} badge ${open ? 'badge-accent' : 'badge-secondary'} aspect-square rounded-full p-[0.2rem] m-0 h-6 w-6 cursor-pointer`}
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d={open ? openSvg : closeSvg}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={3}
+          />
+        </svg>
+      )}
     </>
   );
 
   return (
     <>
       <button
-        className="btn btn-primary pl-3 pr-2 flex lg:hidden"
+        className={`${mobileOnly} ${buttonStyle}`}
         onClick={() =>
           scrollRef?.current?.scrollIntoView({ behavior: 'smooth' })
         }
@@ -56,7 +59,7 @@ export function ShowLinksButton({
         {contents}
       </button>
       <button
-        className="btn btn-primary pl-3 pr-2 hidden lg:flex"
+        className={`${desktopOnly} ${buttonStyle}`}
         onClick={open ? () => setShowLinks(true) : () => setShowLinks(false)}
       >
         {contents}
