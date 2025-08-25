12fb42fdd2a68f475e7dd0c749546e7ee6d1e776
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-08-24 23:14:29 -0400

    implement frontend  redirect

--- src/frontend/react-root.tsx
+++ src/frontend/react-root.tsx
@@ -1,15 +1,38 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+function renderApp() {
+  const rootElement = document.getElementById('root') as HTMLElement;
 
-import { ApiProvider } from '@/frontend/components/ApiProvider';
-import { App } from '@/frontend/components/App';
+  Promise.all([
+    import('react'),
+    import('react-dom/client'),
+    import('@/frontend/components/ApiProvider'),
+    import('@/frontend/components/App'),
+    import('@/frontend/react-root.css'),
+  ]).then(([React, ReactDOMClient, ApiProviderModule, AppModule]) => {
+    const { StrictMode } = React;
+    const { createRoot } = ReactDOMClient;
+    const ApiProvider = ApiProviderModule.ApiProvider;
+    const App = AppModule.App;
 
-import '@/frontend/react-root.css';
+    createRoot(rootElement).render(
+      <StrictMode>
+        <ApiProvider>
+          <App />
+        </ApiProvider>
+      </StrictMode>,
+    );
+  });
+}
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <ApiProvider>
-      <App />
-    </ApiProvider>
-  </StrictMode>,
-);
+(function () {
+  const pathname = window.location.pathname;
+
+  if (pathname === '/' || pathname.startsWith('/links')) {
+    renderApp();
+  } else {
+    try {
+      fetch(`${import.meta.env.VITE_BACKEND_URL}${window.location.pathname}`);
+    } catch {
+      renderApp();
+    }
+  }
+})();
